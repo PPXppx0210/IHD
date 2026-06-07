4d67b664e38e497f028c2023945522f872752950
PPXppx0210 <130343620+PPXppx0210@users.noreply.github.com>
2026-06-07 20:08:46 +0800

    Add files via upload

---
+++ utils/tweet_clustering.py
@@ -0,0 +1,156 @@
+from utils.utils import DATA_ROOTPATH, load_pickle, save_pickle
+from utils.log import logger
+from utils.graph import reindex_graph
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import os
+import random
+from yellowbrick.cluster import KElbowVisualizer
+from sklearn.cluster import KMeans, AgglomerativeClustering, MiniBatchKMeans
+from sklearn.neighbors import NearestCentroid, KNeighborsClassifier
+from sklearn.decomposition import PCA
+from matplotlib import colors
+from typing import Tuple
+
+def get_centroids(X, y):
+    nb_kind = len(np.unique(y))
+    centers_ = []
+    for idx in range(nb_kind):
+        center = np.median(X[y==idx],axis=0)
+        centers_.append([elem for elem in center])
+    return np.array(centers_)
+
+def get_centroids2(X, y):
+    clf = NearestCentroid()
+    clf.fit(X, y)
+    centroids_ = clf.centroids_
+    return np.array([[elem for elem in center] for center in centroids_])
+
+def apply_clustering_algo(tweet_features, model:str='agg', desc='', thr=None, todraw=False):
+    """
+    NOTE: desc的一般形式为f"g{g.vcount()}", 而当model='agg'时形式为f"g{g.vcount()}_thr{thr}"
+    """
+    full_desc = f"{desc}"
+    if model == 'agg':
+        if thr is None:
+            thr = 0.5 # default
+        full_desc += f"_thr{thr}"
+    centroids_filepath = os.path.join(DATA_ROOTPATH, f"HeterGAT/clustering/{full_desc}_{model}_centroids_.pkl")
+    labels_filepath = os.path.join(DATA_ROOTPATH, f"HeterGAT/clustering/{full_desc}_{model}_labels_.pkl")
+    logger.info(f"centroids={centroids_filepath}, labels={labels_filepath}")
+    if os.path.exists(centroids_filepath) and os.path.exists(labels_filepath):
+        return load_pickle(centroids_filepath), load_pickle(labels_filepath)
+
+    if not isinstance(tweet_features, np.ndarray):
+        tweet_features = np.array(tweet_features)
+    
+    if todraw:
+        # Reduce Dimensions for plotting
+        pca_ = PCA(n_components=3)
+        tweet_features = pca_.fit_transform(tweet_features)
+    
+    # 1. sample a fraction of tweets for clustering and training cluster-classifier
+    frac = 1e-3
+    train_tweets = random.choices(tweet_features, k=int(len(tweet_features)*frac))
+    train_tweets_df = pd.DataFrame(train_tweets)
+    logger.info("--Finish Sampling Tweets--")
+
+    # 3. perform clustering
+    if model == 'agg':
+        logger.info(f"--Perform Hierarchical Clustering Method (thr={thr})--")
+        model_ = AgglomerativeClustering(n_clusters=None, distance_threshold=thr) # thr is set to <default> 
+        labels_ = model_.fit_predict(train_tweets_df).astype("int")
+        # 3.2 train classifier and give labels to other test data
+        knnmodel_ = KNeighborsClassifier(n_neighbors=1)
+        knnmodel_.fit(train_tweets_df, labels_)
+        whole_labels_ = knnmodel_.predict(tweet_features)
+        logger.info(f"--Finish Training KNNModel and Calculating {model.capitalize()} Labels--")
+        # 3.1 get centroids
+        centroids = get_centroids2(X=tweet_features, y=whole_labels_)
+        logger.info(f"--Finish Calculating {model.capitalize()} Centroids (Num={model_.n_clusters_}, Shape={centroids.shape})--")
+    elif model == 'mbk':
+        # 2. determine the K
+        elbow_ = KElbowVisualizer(MiniBatchKMeans(), k=(4,30))
+        elbow_.fit(train_tweets_df)
+        n_clusters = elbow_.elbow_value_
+        logger.info(f"--Finish Determining K={n_clusters}--")
+
+        model_ = MiniBatchKMeans(n_clusters=n_clusters, random_state=2023)
+        # 3.2 train classifier and give labels to all data
+        whole_labels_ = model_.fit_predict(tweet_features).astype("int")
+        # 3.1 get centroids
+        centroids = model_.cluster_centers_
+        logger.info(f"--Finish Calculating {model.capitalize()} Labels and Centroids--")
+    save_pickle(centroids, centroids_filepath)
+    save_pickle(whole_labels_, labels_filepath)
+
+    # 4. plot
+    if todraw:
+        fig = plt.figure(figsize=(10,8))
+        ax = fig.add_subplot(111, projection="3d", label="bla", computed_zorder=False)
+        cmap = colors.ListedColormap(["#682F2F", "#9E726F", "#D6B2B1", "#B9C0C9", "#9F8A78", "#F3AB60"])
+        ax.scatter(train_tweets_df[0], train_tweets_df[1], train_tweets_df[2], c=labels_, marker=".", cmap=cmap, zorder=0)
+        ax.scatter(centroids[:,0],centroids[:,1],centroids[:,2], c='red', marker="o", s=100)
+        plt.show()
+    
+    return centroids, whole_labels_
+
+def get_tweet_feat_for_user_nodes(model='lda', num_topics=25) -> np.ndarray:
+    if model == 'lda':
+        twft_filepath = os.path.join(DATA_ROOTPATH, f"HeterGAT/lda-model/twft_per_user_k{num_topics}.pkl")
+    elif model == 'bertopic':
+        twft_filepath = os.path.join(DATA_ROOTPATH, f"HeterGAT/tweet-embedding/bertopic/twft_per_user.pkl")
+    if os.path.exists(twft_filepath):
+        logger.info(f"Trying to Get Twft in path:{twft_filepath}...Success")
+        return load_pickle(twft_filepath)
+    
+    twft = []
+    if model == 'lda':
+        prefix, suffix = os.path.join(DATA_ROOTPATH, "HeterGAT/lda-model"), f"_k{num_topics}_maxiter{50}"
+        logger.info(f"Calculating Twft Using CountVectorizer/LDAModel/ProcessedUserTexts in path:{prefix}/.../{suffix}")
+        for part in range(1,11):
+            cv_ = load_pickle(f"{prefix}/cv/cv_0{part}{suffix}.p")
+            lda_model_ = load_pickle(f"{prefix}/model/model_0{part}{suffix}.p")
+            user_texts_l = load_pickle(f"{prefix}/processedtexts-per-user/ProcessedTexts_{part}.p")
+            twft.extend(lda_model_.transform(cv_.transform(user_texts_l)))
+    elif model == 'bertopic':
+        prefix = os.path.join(DATA_ROOTPATH, "HeterGAT/lda-model")
+        model = load_pickle(os.path.join(DATA_ROOTPATH, "HeterGAT/tweet-embedding/bertopic/topic_approx_distribution_reduce_auto_merge_lt01_subg.pkl"))
+        for part in range(1,11):
+            user_texts_l = load_pickle(f"{prefix}/processedtexts-per-user/ProcessedTexts_{part}.p")
+            topic_distr, _ = model.approximate_distribution(user_texts_l).astype()
+            topic_distr = topic_distr.astype(np.float16) # save space and s/l time
+            twft.extend(topic_distr)
+    twft = np.array(twft)
+    save_pickle(twft, twft_filepath)
+    return twft
+
+def tweet_centralized_process(homo_g, user_tweet_mp, tweet_features, embedding_method, clustering_algo, distance_threshold)->Tuple[dict,list,np.ndarray]:
+    # 1. prepare tweet feature
+    lda_k = tweet_features.shape[1]
+
+    # 2. apply clustering algo
+    centroids_, tw2centroids_ = apply_clustering_algo(tweet_features, model=clustering_algo, desc=f"g{homo_g.vcount()}_emb{embedding_method}", thr=distance_threshold)
+
+    # replace user_tweet_edges with user_tweet_centroid_edges
+    user_nodes = homo_g.vs["label"]
+    user_tweet_centroid_edges = []
+    for user in user_nodes:
+        for tweet in user_tweet_mp[user]:
+            user_tweet_centroid_edges.append((user, tw2centroids_[tweet]))
+
+    # 3. build new homo graph
+    tweet_nodes = [elem for elem in range(len(centroids_))]
+    uu_edges = homo_g.get_edgelist()
+    logger.info(f"Graph Info: nb-users={len(user_nodes)}, nb-tweets={len(tweet_nodes)}, nb-uu-edges={len(uu_edges)}, nb-ut-edges={len(user_tweet_centroid_edges)}")
+    nodes, edges = reindex_graph([user_nodes, tweet_nodes], [uu_edges, user_tweet_centroid_edges])
+
+    # 4. get node features for both users and tweets
+    twft_for_users  = get_tweet_feat_for_user_nodes(model=embedding_method, num_topics=lda_k)
+    if not isinstance(twft_for_users, np.ndarray):
+        twft_for_users = np.array(twft_for_users)
+    feats = np.concatenate((twft_for_users[user_nodes], centroids_))
+    logger.info(f"New Homo Graph Info: nb-nodes={len(nodes)}, nb-edges={len(edges[0])}:{len(edges[1])}, nb-tweets={feats.shape}")
+
+    return nodes, edges, feats
---
+++ utils/tweet_embedding.py
@@ -0,0 +1,310 @@
+from utils.utils import DATA_ROOTPATH, load_pickle, save_pickle, reduce_dimension, sample_docs_foreachuser, sample_docs_foreachuser2
+from utils.log import logger
+import random
+import os
+import numpy as np
+import pandas as pd
+from typing import List
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.decomposition import LatentDirichletAllocation
+import pyLDAvis.sklearn
+import bitermplus
+import torch
+from gsdmm import MovieGroupProcess
+from bertopic import BERTopic
+from sentence_transformers import SentenceTransformer
+from sklearn.feature_extraction import text
+from nltk.corpus import stopwords
+from cuml.cluster import HDBSCAN
+from cuml.manifold import UMAP
+from gensim.models.coherencemodel import CoherenceModel
+from gensim import corpora
+from octis.evaluation_metrics.coherence_metrics import Coherence
+from octis.evaluation_metrics.diversity_metrics import TopicDiversity
+from transformers import AutoTokenizer, AutoModelForMaskedLM
+
+def lda_model(raw_texts, num_topics, visualize):
+    cv = CountVectorizer(stop_words="english")
+    dtm = cv.fit_transform(raw_texts)
+    model = LatentDirichletAllocation(n_components=num_topics, max_iter=50, random_state=2023)
+    model.fit(dtm)
+    topic_distr = model.transform(dtm)
+    if visualize:
+        panel = pyLDAvis.sklearn.prepare(model, dtm, cv, mds='tsne') # Create the panel for the visualization
+    else:
+        panel = None
+    return {
+        "topic-distr": topic_distr,
+        "model": model,
+        "cv": cv,
+        "dtm": dtm,
+        "pyvis-panel": panel,
+    }
+
+def btmplus_model(raw_texts, num_topics, visualize):
+    cv = CountVectorizer(stop_words="english")
+    dtm = cv.fit_transform(raw_texts)
+    vocab = np.array(cv.get_feature_names_out()) # get_feature_names_out is only available in ver1.0
+    
+    # replace words with its vocab word_ids
+    docs_vec = bitermplus.get_vectorized_docs(raw_texts, vocab)
+    biterms = bitermplus.get_biterms(docs_vec)
+    model = bitermplus.BTM(dtm, vocab, T=num_topics)
+    model.fit(biterms, iterations=30)
+
+    topic_distr = model.transform(docs_vec)
+    logger.info(f"METRICS: perplexity={model.perplexity_}, coherence={model.coherence_}") # model.labels_
+    return {
+        "topic-distr": topic_distr,
+        "model": model,
+        "cv": cv,
+        "dtm": dtm,
+        "pyvis-panel": None,
+    }
+
+def gsdmm_model(raw_texts, num_topics, visualize):
+    # NOTE: we cant first sample train-corpus, bcz vocabs extracted from train-corpus is not enough for transforming the whole train+test corpus
+    cv = CountVectorizer(stop_words="english")
+    dtm = cv.fit_transform(raw_texts)
+    n_docs, n_terms = dtm.shape
+    model = MovieGroupProcess(K=num_topics, alpha=0.01, beta=0.01, n_iters=30)
+    docs = [doc.split(' ') for doc in docs] # NOTE: Important!!!, since our sentence is string"Absolutely", if we dont split it, `for word in doc` would produce 'A b s...'
+    # NOTE: around 1min/10,0000docs
+    labels = model.fit(docs, n_terms)
+
+    # get topic distribution
+    # NOTE: we can rewrite mgp.py in gsdmm to save intermediate variables, i.e. m_z, n_z, n_z_w, d_z
+    # model = MovieGroupProcess().from_data(K=20, alpha=0.01, beta=0.01, D=n_docs, vocab_size=n_terms, 
+    #     cluster_doc_count=m_z, cluster_word_count=n_z, cluster_word_distribution=n_z_w)
+    topic_distrs = []
+    for doc in raw_texts: # NOTE: around 1min/10,0000docs
+        topic_distrs.append(model.score(doc))
+    return {
+        "topic-distr": topic_distrs,
+        "model": model,
+        "cv": cv,
+        "dtm": dtm,
+        "pyvis-panel": None,
+    }
+
+def bertopic_model(raw_texts, num_topics, visualize):
+    """
+    Arguments
+        raw_texts:  lists of raw texts
+        num_topics: NO USE HERE
+    >>> raw_texts = load_pickle("/remote-home/share/dmb_nas/ppx/HeterGAT/tweet-embedding/bertopic/raw_texts_aggby_user_filter_lt2words_processedforbert_subg.pkl")
+    >>> raw_texts = [item for sublist in sample_docs for item in sublist]
+    """
+    train_texts = sample_docs_foreachuser2(raw_texts, sample_frac=0.1)
+    sentence_model = SentenceTransformer("all-MiniLM-L6-v2")
+    embeddings = sentence_model.encode(train_texts, show_progress_bar=True)
+
+    # Create instances of GPU-accelerated UMAP and HDBSCAN
+    umap_model = UMAP(n_components=5, n_neighbors=15, min_dist=0.0)
+    hdbscan_model = HDBSCAN(min_samples=10, gen_min_span_tree=True, prediction_data=True)
+
+    # Pass the above models to be used in BERTopic
+    # stop_words = set(text.ENGLISH_STOP_WORDS)
+    # stop_words |= set(stopwords.words('english'))
+    # stop_words |= set(['people'])
+    # vectorizer_model = CountVectorizer(ngram_range=(1, 3), stop_words=list(stop_words))
+    vectorizer_model = CountVectorizer(ngram_range=(1, 3), stop_words='english')
+    model = BERTopic(language="multilingual", nr_topics="auto", top_n_words=15, min_topic_size=20,
+        embedding_model=sentence_model, vectorizer_model=vectorizer_model,
+        umap_model=umap_model, hdbscan_model=hdbscan_model
+    )
+
+    # NOTE: fit on 1/10 dataset takes ~10min, ~8G(13G on the peak) RAM
+    topic_model = model.fit(train_texts, embeddings)
+    # topic_model.reduce_topics(raw_texts, nr_topics=50)
+
+    # NOTE: transform on whole dataset takes ~2h, 1/4 takes 30min
+    N_FRAC, n_docs = 4, len(raw_texts)
+    topic_distr_all, topic_label_all, topic_prob_all = [], [], []
+    for frac in range(N_FRAC):
+        test_texts = raw_texts[int(n_docs/N_FRAC*frac):int(n_docs/N_FRAC*(frac+1))]
+        topic_distr, _ = topic_model.approximate_distribution(test_texts, min_similarity=1e-5)
+        topic_distr = topic_distr.astype(np.float16) # save space and s/l time
+        labels, probs = topic_model.transform(test_texts)
+        topic_distr_all.extend(topic_distr); topic_label_all.extend(labels); topic_prob_all.extend(probs)
+    
+    # NOTE: update topics and reduce outliers in class '-1'
+    # new_topics = topic_model.reduce_outliers(raw_texts, topics, probabilities=topic_distr, strategy="probabilities")
+    # topic_model.update_topics(raw_texts, new_topics)
+
+    # NOTE: Visualize All Topics in One Distance-Map
+    # topic_model.visualize_topics()
+    # NOTE: Visualize All Document-Embeddings aggby Clusters
+    # topic_model.visualize_documents(sample_docs, embeddings=embeddings, hide_document_hover=False)
+
+    # NOTE: Get Top-N Words per Topic
+    # topic_model.topic_representations_
+    # NOTE: Get Topic-Word Matrix
+    # topic_model.c_tf_idf
+
+    return {
+        "topic-distr": topic_distr_all,
+        "model": topic_model,
+        "cv": None,
+        "dtm": None,
+        "pyvis-panel": None,
+    }
+
+def merge_similar_topics(topic_model:BERTopic, docs:List, merge_thr:float=0.1) -> BERTopic:
+    # reduce topics
+    topic_model.reduce_topics(docs, nr_topics="auto")
+
+    # merge topics
+    hierarchical_topics = topic_model.hierarchical_topics(docs)
+    merge_ids = [row['Topics'] for _, row in hierarchical_topics.iterrows() if row['Distance']<merge_thr]
+    topic_model.merge_topics(docs, merge_ids)
+    # Visualize Hierarchical Topics
+    # topic_model.get_topic_tree(hierarchical_topics)
+    return topic_model
+
+def update_topic_reprs(topic_model:BERTopic, docs:List, ) -> BERTopic:
+    import emoji
+    demojize_texts = ['_'+demojize_text[1:-1]+'_' for demojize_text in emoji.get_emoji_unicode_dict(lang='en').keys()]
+
+    from sklearn.feature_extraction.text import ENGLISH_STOP_WORDS
+    # from nltk.corpus import stopwords
+    # stop_words_list = stopwords.words('english') # 鑾峰彇鑻辨枃鍋滅敤璇嶅垪琛?
+    stop_words = set(ENGLISH_STOP_WORDS) | set(demojize_texts)
+    logger.info(f"stop_words={len(stop_words)}, ENGLISH_STOP_WORDS={len(ENGLISH_STOP_WORDS)}, demojize_texts={len(demojize_texts)}")
+    vectorizer_model = CountVectorizer(ngram_range=(1,3), stop_words=list(stop_words))
+
+    topic_model.update_topics(docs=docs, vectorizer_model=vectorizer_model)
+    return topic_model
+
+def calculate_coherence_score(docs, topics, topic_model:BERTopic):
+    """
+    How to get Args, i.e. docs, topics and topic_model? Use Bertopic as example...
+    >>> topic_model = model.fit(docs, embeddings)
+    >>> topic_distr, _ = topic_model.approximate_distribution(docs, min_similarity=1e-5)
+    >>> topics, probs = topic_model.transform(docs)
+    """
+
+    # Preprocess Documents
+    documents = pd.DataFrame({"Document": docs,
+        "ID": range(len(docs)),
+        "Topic": topics})
+    documents_per_topic = documents.groupby(['Topic'], as_index=False).agg({'Document': ' '.join})
+    cleaned_docs = topic_model._preprocess_text(documents_per_topic.Document.values)
+
+    # Extract vectorizer and tokenizer from BERTopic
+    vectorizer = topic_model.vectorizer_model
+    tokenizer = vectorizer.build_analyzer() # can be used for n-gram words tokenizing
+
+    # Extract features for Topic Coherence evaluation
+    # all_words = vectorizer.get_feature_names_out()
+    all_words = vectorizer.get_feature_names()
+    tokens = [tokenizer(doc) for doc in cleaned_docs]
+    dictionary = corpora.Dictionary(tokens)
+    corpus = [dictionary.doc2bow(token) for token in tokens]
+
+    # Create topic words
+    topic_words = [topic_model.get_topic(topic) for topic in range(len(topic_model.topic_labels_))]
+    topic_words = [[w[0] for w in words if w[0] in all_words] for words in topic_words if words]
+
+    coherence_model = CoherenceModel(topics=topic_words,
+        texts=tokens,
+        corpus=corpus,
+        dictionary=dictionary,
+        coherence='c_v')
+    # coherence_model_npmi = CoherenceModel(topics=topic_words,
+    #     texts=tokens,
+    #     corpus=corpus,
+    #     dictionary=dictionary,
+    #     coherence='c_npmi')
+    # coherence_model_npmi.get_coherence()
+    npmi = Coherence(texts=tokens, topk=topic_model.top_n_words, measure='c_npmi')
+    topic_diversity = TopicDiversity(topk=topic_model.top_n_words)
+    results = {
+        "gensim.coherence": coherence_model.get_coherence(),
+        "octis.npmi": npmi.score({"topics":topic_words}),
+        "octis.topic_diversity": topic_diversity.score({"topics":topic_words}),
+    }
+    return results
+
+def get_tweet_feat_for_tweet_nodes(model="lda", num_topics=20, visualize=False):
+    """
+    valid model names are ['lda','btm','gsdmm','bertopic']
+    """
+    suffix = f"model{model}" # used for naming saved-results, i.e. topic-distr_{suffix}.pkl
+    if model == 'lda' or model == 'btm':
+        suffix += f"_numtopic{num_topics}"
+
+    sent_emb_dir = os.path.join(DATA_ROOTPATH, "HeterGAT/lda-model/")
+    raw_texts = load_pickle(os.path.join(sent_emb_dir, "processedtexts-per-text/ptexts_per_user_all.p"))
+    if model == "lda":
+        ret = lda_model(raw_texts=raw_texts, num_topics=num_topics, visualize=visualize)
+    elif model == "btm":
+        # ret = btm_model(raw_texts=raw_texts, num_topics=num_topics, visualize=visualize)
+        ret = btmplus_model(raw_texts=raw_texts, num_topics=num_topics, visualize=visualize)
+    elif model == 'gsdmm':
+        ret = gsdmm_model(raw_texts=raw_texts, num_topics=num_topics, visualize=visualize)
+    elif model == 'bertopic':
+        # NOTE: there are two ways for creating topic distributions, see details in reference https://github.com/MaartenGr/BERTopic/issues/1026
+        # we choose to use func approximate_distribution(), since it is fasttttt!
+        # Other options for accelaerating can be referenced in https://maartengr.github.io/BERTopic/getting_started/tips_and_tricks/tips_and_tricks.html#gpu-acceleration
+        ret = bertopic_model(raw_texts=raw_texts, num_topics=num_topics, visualize=visualize)
+    
+    # Save Final Results
+    for key,val in ret.items():
+        if val is None:
+            continue
+        save_pickle(val, f"{key}_{suffix}.pkl")
+
+def agg_tagemb_by_user(n_user:int, cascades:dict, pretrained_model_name:str='xlm-roberta-base')->np.ndarray:
+    # Prepare Pretrained-LLM
+    tokenizer = AutoTokenizer.from_pretrained(pretrained_model_name)
+    model = AutoModelForMaskedLM.from_pretrained(pretrained_model_name)
+    if torch.cuda.is_available():
+        model = model.to('cuda')
+
+    # Per Hashtag
+    tag_embs = []
+    # tag2emb = {}
+    user2tagids = [[] for _ in range(n_user)]
+    for tag_id, tag in enumerate(sorted(cascades.keys())):
+        # Calculate Emb
+        with torch.no_grad():
+            tokens = tokenizer(tag, return_tensors='pt', padding=True)
+            if torch.cuda.is_available():
+                tokens = {
+                    'input_ids': tokens['input_ids'].to('cuda'),
+                    # 'token_type_ids': tokens['token_type_ids'].to('cuda'),
+                    'attention_mask': tokens['attention_mask'].to('cuda'),
+                }
+            output = model(**tokens)
+            embeds = output.logits.detach().cpu().numpy()
+            embeds = np.mean(embeds[0], axis=0, dtype=np.float16)
+        tag_embs.append(embeds)
+        # tag2emb[tag] = embeds
+        del tokens, output
+
+        # Accumulate User Appearance in Multiple Tags
+        cascade = cascades[tag]
+        for user_id, _ in cascade:
+            user2tagids[user_id].append(tag_id)
+    tag_embs = np.array(tag_embs)
+    save_pickle(tag_embs, os.path.join(DATA_ROOTPATH, "Weibo-Aminer/llm/tag_embs_model_xlm-roberta-base.pkl"))
+    
+    # Aggregate Tag Emb foreach User
+    user2emb = []
+    for tag_ids in user2tagids:
+        if len(tag_ids) != 0:
+            emb = np.mean(tag_embs[tag_ids], axis=0)
+        else:
+            emb = np.zeros(shape=tag_embs.shape[1])
+        user2emb.append(emb)
+    user2emb = np.array(user2emb)
+    save_pickle(user2emb, os.path.join(DATA_ROOTPATH, "Weibo-Aminer/llm/tag_embs_aggbyuser_model_xlm-roberta-base.pkl"))
+
+    # TODO: Reduce LLM Dimension
+    # NOTE: 1. Use Traditional dimension reduction methods, i.e. PCA; 2. Add FC-Layer in Total Model
+    if pretrained_model_name == 'xlm-roberta-base':
+        user2emb = reduce_dimension(user2emb, reduce_dim=128)
+    save_pickle(user2emb, os.path.join(DATA_ROOTPATH, "Weibo-Aminer/llm/tag_embs_aggbyuser_model_xlm-roberta-base_pca_dim128.pkl"))
+    
+    return user2emb
